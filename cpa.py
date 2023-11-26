37e36e1fb0807e0f8c1ff9663dfb53601255b4d5
René Gál <galrene@fit.cvut.cz>
2023-11-26 21:54:02 +0100

    cpa: more checks, load traces into matrix

--- cpa.py
+++ cpa.py
@@ -4,7 +4,16 @@ import numpy as np
 
 
 class Measurement:
+    """
+    Class encapsulating an oscilloscope measurement, it's corresponding plaintext and ciphertext
+    for a power analysis attack.
+    """
+
     def __init__(self, plaintext: str, ciphertext: str, trace: str):
+        if (not os.path.isfile(plaintext)
+                or not os.path.isfile(ciphertext)
+                or not os.path.isfile(trace)):
+            raise FileNotFoundError(f"The file '{plaintext}' was not found.")
         self.plaintext_path = plaintext
         self.ciphertext_path = ciphertext
         self.trace_path = trace
@@ -19,6 +28,7 @@ class Measurement:
         with open(self.plaintext_path, 'r') as file:
             pt_line_count = sum(1 for _ in file)
         if trace_size % pt_line_count != 0:
+            print(f"Trace size: {trace_size}\nPT line count: {pt_line_count}")
             raise ValueError("Binary data size is not a multiple of PT line count")
         return trace_size // pt_line_count
 
@@ -44,19 +54,15 @@ def load_trace_length_file(file_path):
 
 
 def main():
-    knownKeyMeasurement = Measurement(
-        plaintext='cpa/plaintext-00112233445566778899aabbccddeeff.txt',
-        ciphertext='cpa/ciphertext-00112233445566778899aabbccddeeff.txt',
-        trace='cpa/traces-00112233445566778899aabbccddeeff.bin'
-    )
-    unknownKeyMeasurement = Measurement(
+    unknown_key_measurement = Measurement(
         plaintext='cpa/plaintext-unknown_key.txt',
         ciphertext='cpa/ciphertext-unknown_key.txt',
         trace='cpa/traces-unknown_key.bin'
     )
 
-    # Create a NumPy array
-    matrix = np.frombuffer(binary_data, dtype=np.int8).reshape(-1, elements_per_row)
+    binary_data = load_binary_file(unknown_key_measurement.trace_path)
+    matrix = (np.frombuffer(binary_data, dtype=np.int8).
+              reshape(-1, unknown_key_measurement.trace_length))
 
     print(matrix)
 
