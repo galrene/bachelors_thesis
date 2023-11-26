692f53f5183195c455c52b5ca30453b9b0c15e84
René Gál <galrene@fit.cvut.cz>
2023-11-26 21:25:46 +0100

    cpa: calculate trace length

--- cpa.py
+++ cpa.py
@@ -1,19 +1,49 @@
+import os
+
 import numpy as np
+
+
+def get_file_size(file_path):
+    try:
+        size = os.path.getsize(file_path)
+        return size
+    except FileNotFoundError:
+        print(f"The file '{file_path}' was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 
 def load_binary_file(file_path):
     with open(file_path, 'rb') as binary_file:
         data = binary_file.read()
     return data
 
+
 def load_trace_length_file(file_path):
     with open(file_path, 'r') as trace_length_file:
         return int(trace_length_file.readline()[1:])  # skip the + character
 
-def main():
-    binary_file_path = 'traces-00112233445566778899aabbccddeeff.bin'
-    trace_length_file_path = 'traceLength-unknown_key.txt'
 
-    trace_length = load_trace_length_file(trace_length_file_path)
+def get_trace_length(bin_file, pt_file):
+    """
+    bin_file: path to measurement.bin file
+    pt_file: path to plaintext file
+    Returns length of each trace by dividing the size of the binary file
+    by the amount of measurements.
+    """
+    bin_size = get_file_size(bin_file)
+    with open(pt_file, 'r') as file:
+        pt_line_count = sum(1 for _ in file)
+    if bin_size % pt_line_count != 0:
+        raise ValueError("Binary data size is not a multiple of PT line count")
+    return bin_size // pt_line_count
+
+
+def main():
+    binary_file_path = 'cpa/traces-00112233445566778899aabbccddeeff.bin'
+    pt_file_path = 'cpa/plaintext-00112233445566778899aabbccddeeff.txt'
+
+    trace_length = get_trace_length(binary_file_path, pt_file_path)
     binary_data = load_binary_file(binary_file_path)
     elements_per_row = len(binary_data) // trace_length
 
